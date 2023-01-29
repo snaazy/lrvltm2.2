183bdc84784c039c86c72024f822c166f879b2b0
Yassine FEKIH HASSEN <yassinefekih78@outlook.fr>
2023-01-29 17:18:12 +0100

    Qlq modifs

--- resources/views/departements.blade.php
+++ resources/views/departements.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>TM2 - EXO 2</title>
 
     </head>
     <body>
