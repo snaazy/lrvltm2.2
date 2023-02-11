4900eaf80ec600154dd8a6c343e7ad5427b21cdb
Yassine FEKIH HASSEN <yassinefekih78@outlook.fr>
2023-02-11 16:37:44 +0100

    Final

--- resources/views/departements.blade.php
+++ resources/views/departements.blade.php
@@ -1,37 +1,32 @@
 @extends('layout.app')
 
-@section('title')
+@section('title','Exo 2')
 
-TM2
-
-@endsection
 
 @section('header')
-
-<a class="navbar-brand" href="#">Programmation Web - TM2 : Exercice 2 </a>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center">
+  <a class="navbar-brand" href="#">Programmation Web - TM2 : Exercice 2</a>
+</nav>
 @endsection
 
 @section('content')
-
-<table class="table">
+<div class="container mt-5">
+  <h2 class="text-center mb-4">Liste des départements</h2>
+  <table class="table table-bordered table-hover shadow-sm">
     <thead>
-        <tr>
-            <th>No</th>
-            <th>Nom</th>
-        </tr>
+      <tr>
+        <th>No</th>
+        <th>Nom</th>
+      </tr>
     </thead>
     <tbody>
-        @foreach($departements as $departement)
-        <tr>
-            <td class="align-middle">{{ $departement->no }}</td>
-            <td class="align-middle">{{ $departement->nom }}</td>
-        </tr>
-        @endforeach
+      @foreach($departements as $departement)
+      <tr>
+        <td class="align-middle">{{ $departement->no }}</td>
+        <td class="align-middle">{{ $departement->nom }}</td>
+      </tr>
+      @endforeach
     </tbody>
-</table>
-
+  </table>
+</div>
 @endsection
-
-
-
